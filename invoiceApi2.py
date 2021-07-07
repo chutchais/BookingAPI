c00cec379824d7df0a8e437c90c58efdc4b32da4
chutchais <chutchai.s@gmail.com>
2021-07-07 12:54:12 +0700

    Fix incorrect tariff for import

--- invoiceApi2.py
+++ invoiceApi2.py
@@ -191,29 +191,42 @@ def db_ctcs_get_receivedetail_by_invoice(invoice):
     #import datetime
     
     cursor_ctcs = init_db()
-    cursor_ctcs.execute("select IDTXUD tariff_name1 ,IDT3UD tarliff_name2,"\
-                    "sum(AEHD0C) qty,max(TARF0C) unit_price,sum(TAR10C) amount,max(MTKD0C) currency,"\
-                    "max(BOF10C) total_charge,max(BBT10C) vat,max(BTF00C) grand_total,max(USPCUF) user "
-                    "from LCB1SRC.ETAXDETAIL  "\
-                    "where NFK00C='" + invoice + "' "\
-                    "group by IDTXUD,IDT3UD ")
+    # cursor_ctcs.execute("select IDTXUD tariff_name1 ,IDT3UD tarliff_name2,"\
+    #                 "sum(AEHD0C) qty,max(TARF0C) unit_price,sum(TAR10C) amount,max(MTKD0C) currency,"\
+    #                 "max(BOF10C) total_charge,max(BBT10C) vat,max(BTF00C) grand_total,max(USPCUF) user "
+    #                 "from LCB1SRC.ETAXDETAIL  "\
+    #                 "where NFK00C='" + invoice + "' and EENH0C='CNT' "\
+    #                 "group by IDTXUD,IDT3UD,TARF0C")
+
+    cursor_ctcs.execute("select t.tariff_name1 ,t.tariff_name2,"\
+                        "sum(t.qty) qty ,max(t.unit_price) unit_price,sum(t.qty)*max(t.unit_price) amount,"\
+                        "max(t.currency) currency,max(t.total_charge) total_charge,max(t.vat) vat,max(t.grand_total) grand_total,"\
+                        "max(t.user) user "\
+                        "from ("\
+                        "select IDTXUD tariff_name1 ,IDT3UD tariff_name2, "\
+                            "max(AEHD0C) qty,max(TARF0C) unit_price,'0' amount,max(MTKD0C) currency,"\
+                            "max(BOF10C) total_charge,max(BBT10C) vat,max(BTF00C) grand_total,max(USPCUF) user "\
+                            "from LCB1SRC.ETAXDETAIL  "\
+                            "where NFK00C='" + invoice + "' "\
+                            "group by VFID0C,IDTXUD,IDT3UD,TARF0C "\
+                        ") t "\
+                        "group by t.tariff_name1,t.tariff_name2,t.unit_price "\
+                        "order by t.tariff_name1,t.tariff_name2,t.unit_price")
 
     rows = cursor_ctcs.fetchall()
+    
     columns = [column[0].lower() for column in cursor_ctcs.description]
     # print(columns, file=sys.stdout)
     if rows:
-        # print('Found Data ' + hid , file=sys.stdout)
         results = []
         for row in rows:
             clean_d = { k:v.strip() for k, v in zip(columns,row) if isinstance(v, str)}
-            
             clean_date = { k:v for k, v in zip(columns,row) if isinstance(v, decimal.Decimal)}
-           
             clean_d.update(clean_date)
             results.append(dict(clean_d))
 
-        # print(results, file=sys.stdout)
-        # results[0]
+        # Check Is it IMPORT?? , if yes ,we must have STORAGE charge
+
         total_text_en = number2text(str(results[0]['grand_total']),'en')
         total_text_th = number2text(str(results[0]['grand_total']),'th')
         final_result={'currency':results[0]['currency'],
@@ -223,6 +236,7 @@ def db_ctcs_get_receivedetail_by_invoice(invoice):
                     'grand_text_en':total_text_en,
                     'grand_text_thai':total_text_th,
                     'user':results[0]['user'] }
+
         final_result['details'] = reorder_tariff(results)
         return final_result
 
